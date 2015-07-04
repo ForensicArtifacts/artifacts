e5952355b3791105299b9c1ca2077da99634bc8e
Joachim Metz <joachim.metz@gmail.com>
2015-07-04 09:12:04 +0200

    Added update version scripts.

---
+++ utils/update_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to update the version information.
+
+DATE_VERSION=`date +"%Y%m%d"`;
+DATE_DPKG=`date -R`;
+EMAIL_DPKG="Forensic artifacts <forensicartifacts@googlegroups.com>";
+
+sed -i -e "s/^\(__version__ = \)'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'$/\1'${DATE_VERSION}'/" artifacts/__init__.py
+sed -i -e "s/^\(python-artifacts \)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-1)/\1(${DATE_VERSION}-1)/" config/dpkg/changelog
+sed -i -e "s/^\( -- ${EMAIL_DPKG}  \).*$/\1${DATE_DPKG}/" config/dpkg/changelog
