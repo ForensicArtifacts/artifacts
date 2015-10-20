ef03b7008af7c4d95cfd7b22ce1fe38e160f2a61
destijl <github@mailgreg.com>
2015-10-20 10:45:19 -0700

    Change error text to undefined keys.

--- artifacts/reader.py
+++ artifacts/reader.py
@@ -74,7 +74,8 @@ class YamlArtifactsReader(ArtifactsReader):
 
     different_keys = set(yaml_definition) - definitions.TOP_LEVEL_KEYS
     if different_keys:
-      raise errors.FormatError(u'Bad keys: {0!s}'.format(different_keys))
+      raise errors.FormatError(u'Undefined keys: {0!s}'.format(
+          different_keys))
 
     name = yaml_definition.get(u'name', None)
     if not name:
