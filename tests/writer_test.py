2d6bfb332e0d393c61486f1ca34e212529f0900a
Sean Gillespie <sean.gillespie.32@gmail.com>
2016-08-08 10:47:30 -0700

    Add JsonArtifactReader and modified Reader/Writer tests

--- tests/writer_test.py
+++ tests/writer_test.py
@@ -11,47 +11,35 @@ from artifacts import reader
 from artifacts import writer
 
 
-class YamlArtifactsWriterTest(unittest.TestCase):
-  """Class to test the YAML artifacts writer."""
+class ArtifactsWriterTest(unittest.TestCase):
+  """Class to test the artifacts writer."""
+
+  def checkArtifactConversion(self, artifact_reader, artifact_writer,
+                              test_file):
+    artifact_definitions = list(artifact_reader.ReadFile(test_file))
+    with tempfile.NamedTemporaryFile() as artifact_file:
+      artifact_writer.WriteArtifactsFile(artifact_definitions,
+                                         artifact_file.name)
+      converted_artifact_definitions = list(artifact_reader.ReadFile(
+          artifact_file.name))
+
+    self.assertListEqual(
+        [artifact.AsDict() for artifact in artifact_definitions],
+        [artifact.AsDict() for artifact in converted_artifact_definitions])
 
   def testYamlWriter(self):
     """Tests the YamlArtifactsWriter FormatArtifacts method for loss during conversion."""
     artifact_reader = reader.YamlArtifactsReader()
     artifact_writer = writer.YamlArtifactsWriter()
     test_file = os.path.join('test_data', 'definitions.yaml')
-
-    artifact_definitions = list(artifact_reader.ReadFile(test_file))
-    with tempfile.NamedTemporaryFile() as artifact_file:
-      artifact_writer.WriteArtifactsFile(artifact_definitions,
-                                         artifact_file.name)
-      converted_artifact_definitions = list(artifact_reader.ReadFile(
-          artifact_file.name))
-
-    self.assertListEqual(
-        [artifact.AsDict() for artifact in artifact_definitions],
-        [artifact.AsDict() for artifact in converted_artifact_definitions])
-
-
-class JsonArtifactsWriterTest(unittest.TestCase):
-  """Class to test the JSON artifacts writer."""
+    self.checkArtifactConversion(artifact_reader, artifact_writer, test_file)
 
   def testJsonWriter(self):
     """Tests the JsonArtifactsWriter FormatArtifacts method for loss during conversion."""
     artifact_reader = reader.JsonArtifactsReader()
     artifact_writer = writer.JsonArtifactsWriter()
     test_file = os.path.join('test_data', 'definitions.json')
-
-    artifact_definitions = list(artifact_reader.ReadFile(test_file))
-    with tempfile.NamedTemporaryFile() as artifact_file:
-      artifact_writer.WriteArtifactsFile(artifact_definitions,
-                                         artifact_file.name)
-
-      converted_artifact_definitions = list(artifact_reader.ReadFile(
-          artifact_file.name))
-
-    self.assertListEqual(
-        [artifact.AsDict() for artifact in artifact_definitions],
-        [artifact.AsDict() for artifact in converted_artifact_definitions])
+    self.checkArtifactConversion(artifact_reader, artifact_writer, test_file)
 
 
 if __name__ == '__main__':
