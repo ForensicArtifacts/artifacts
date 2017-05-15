68d20d5e657345cbfc2f80ff3578126a2bd13978
Joachim Metz <joachim.metz@gmail.com>
2017-05-15 21:41:58 +0200

    Added MacOS and coveralls support to tests

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -19,7 +19,7 @@ PYTHON3_TEST_DEPENDENCIES="python3-yapf";
 # Exit on error.
 set -e;
 
-if test `uname -s` = "Darwin";
+if test ${TRAVIS_OS_NAME} = "osx";
 then
 	git clone https://github.com/log2timeline/l2tdevtools.git;
 
@@ -28,9 +28,15 @@ then
 
 	PYTHONPATH=../l2tdevtools ../l2tdevtools/tools/update.py --download-directory=dependencies ${L2TBINARIES_DEPENDENCIES} ${L2TBINARIES_TEST_DEPENDENCIES};
 
-elif test `uname -s` = "Linux";
+elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "2.7";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON3_TEST_DEPENDENCIES};
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};
+
+elif test ${TRAVIS_OS_NAME} = "linux" && test ${TRAVIS_PYTHON_VERSION} = "3.4";
+then
+	sudo add-apt-repository ppa:gift/dev -y;
+	sudo apt-get update -q;
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON3_TEST_DEPENDENCIES};
 fi
