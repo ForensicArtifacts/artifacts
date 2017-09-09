f385ae9e0b7182a8ad757edcb66e9fc0fbf2eca3
Joachim Metz <joachim.metz@gmail.com>
2017-09-09 11:22:27 +0200

    Fixed version update of config/dpkg/changelog #236

--- utils/update_version.sh
+++ utils/update_version.sh
@@ -6,5 +6,5 @@ DATE_DPKG=`date -R`;
 EMAIL_DPKG="Forensic artifacts <forensicartifacts@googlegroups.com>";
 
 sed -i -e "s/^\(__version__ = \)'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'$/\1'${DATE_VERSION}'/" artifacts/__init__.py
-sed -i -e "s/^\(python-artifacts \)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-1)/\1(${DATE_VERSION}-1)/" config/dpkg/changelog
+sed -i -e "s/^\(artifacts \)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-1)/\1(${DATE_VERSION}-1)/" config/dpkg/changelog
 sed -i -e "s/^\( -- ${EMAIL_DPKG}  \).*$/\1${DATE_DPKG}/" config/dpkg/changelog
