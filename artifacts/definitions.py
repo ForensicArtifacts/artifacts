d65fc6120a4d4849bcabe18e8e5189c16df3b853
Thomas Chopitea <tomchop@gmail.com>
2018-08-22 15:06:59 +0200

    Add Docker label

--- artifacts/definitions.py
+++ artifacts/definitions.py
@@ -36,7 +36,8 @@ LABELS = {
     'Rekall': 'Artifacts using the Rekall memory forensics framework.',
     'Software': 'Installed software.',
     'System': 'Core system artifacts.',
-    'Users': 'Information about users.'
+    'Users': 'Information about users.',
+    'Docker': 'Docker artifacts',
 }
 
 SUPPORTED_OS_DARWIN = 'Darwin'
