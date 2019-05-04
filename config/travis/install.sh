dabcefd662e4c9b20a44d438a9265fc6be5c8e74
Joachim Metz <joachim.metz@gmail.com>
2019-05-04 09:04:38 +0200

    Moved CI tests to Ubuntu 18.04 on Docker

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -11,19 +11,19 @@ L2TBINARIES_TEST_DEPENDENCIES="funcsigs mock pbr six";
 
 DPKG_PYTHON2_DEPENDENCIES="python-yaml";
 
-DPKG_PYTHON2_TEST_DEPENDENCIES="python-coverage python-funcsigs python-mock python-pbr python-six";
+DPKG_PYTHON2_TEST_DEPENDENCIES="python-coverage python-funcsigs python-mock python-pbr python-setuptools python-six";
 
 DPKG_PYTHON3_DEPENDENCIES="python3-yaml";
 
-DPKG_PYTHON3_TEST_DEPENDENCIES="python3-mock python3-pbr python3-setuptools python3-six";
+DPKG_PYTHON3_TEST_DEPENDENCIES="python3-distutils python3-mock python3-pbr python3-setuptools python3-six";
 
 RPM_PYTHON2_DEPENDENCIES="python2-pyyaml";
 
-RPM_PYTHON2_TEST_DEPENDENCIES="python2-funcsigs python2-mock python2-pbr python2-six";
+RPM_PYTHON2_TEST_DEPENDENCIES="python2-funcsigs python2-mock python2-pbr python2-setuptools python2-six";
 
 RPM_PYTHON3_DEPENDENCIES="python3-pyyaml";
 
-RPM_PYTHON3_TEST_DEPENDENCIES="python3-mock python3-pbr python3-six";
+RPM_PYTHON3_TEST_DEPENDENCIES="python3-mock python3-pbr python3-setuptools python3-six";
 
 # Exit on error.
 set -e;
@@ -58,20 +58,31 @@ then
 
 	docker run --name=${CONTAINER_NAME} --detach -i registry.fedoraproject.org/fedora:${FEDORA_VERSION};
 
+	# Install dnf-plugins-core.
 	docker exec ${CONTAINER_NAME} dnf install -y dnf-plugins-core;
 
+	# Add additional dnf repositories.
 	docker exec ${CONTAINER_NAME} dnf copr -y enable @gift/dev;
 
 	if test -n "${TOXENV}";
 	then
-		docker exec ${CONTAINER_NAME} dnf install -y python3-tox;
+		RPM_PACKAGES="python3-tox";
 
-	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
-	then
-		docker exec ${CONTAINER_NAME} dnf install -y git python2 ${RPM_PYTHON2_DEPENDENCIES} ${RPM_PYTHON2_TEST_DEPENDENCIES};
 	else
-		docker exec ${CONTAINER_NAME} dnf install -y git python3 ${RPM_PYTHON3_DEPENDENCIES} ${RPM_PYTHON3_TEST_DEPENDENCIES};
+		RPM_PACKAGES="";
+
+		if test ${TARGET} = "pylint";
+		then
+			RPM_PACKAGES="${RPM_PACKAGES} findutils pylint";
+		fi
+		if test ${TRAVIS_PYTHON_VERSION} = "2.7";
+		then
+			RPM_PACKAGES="${RPM_PACKAGES} python2 ${RPM_PYTHON2_DEPENDENCIES} ${RPM_PYTHON2_TEST_DEPENDENCIES}";
+		else
+			RPM_PACKAGES="${RPM_PACKAGES} python3 ${RPM_PYTHON3_DEPENDENCIES} ${RPM_PYTHON3_TEST_DEPENDENCIES}";
+		fi
 	fi
+	docker exec ${CONTAINER_NAME} dnf install -y ${RPM_PACKAGES};
 
 	docker cp ../artifacts ${CONTAINER_NAME}:/
 
@@ -83,51 +94,54 @@ then
 
 	docker run --name=${CONTAINER_NAME} --detach -i ubuntu:${UBUNTU_VERSION};
 
+	# Install add-apt-repository and locale-gen.
 	docker exec ${CONTAINER_NAME} apt-get update -q;
-	docker exec ${CONTAINER_NAME} sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y locales software-properties-common";
+	docker exec -e "DEBIAN_FRONTEND=noninteractive" ${CONTAINER_NAME} sh -c "apt-get install -y locales software-properties-common";
 
-	docker exec ${CONTAINER_NAME} add-apt-repository ppa:gift/dev -y;
-
-	docker exec ${CONTAINER_NAME} locale-gen en_US.UTF-8;
-
+	# Add additional apt repositories.
 	if test -n "${TOXENV}";
 	then
 		docker exec ${CONTAINER_NAME} add-apt-repository universe;
 		docker exec ${CONTAINER_NAME} add-apt-repository ppa:deadsnakes/ppa -y;
 
-		DPKG_PYTHON="python${TRAVIS_PYTHON_VERSION} python${TRAVIS_PYTHON_VERSION}-dev";
-
-		docker exec ${CONTAINER_NAME} sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential ${DPKG_PYTHON} tox";
-
-	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	elif test ${TARGET} = "pylint";
 	then
-		docker exec ${CONTAINER_NAME} sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y git python ${DPKG_PYTHON2_DEPENDENCIES} ${DPKG_PYTHON2_TEST_DEPENDENCIES}";
-	else
-		docker exec ${CONTAINER_NAME} sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -y git python3 ${DPKG_PYTHON3_DEPENDENCIES} ${DPKG_PYTHON3_TEST_DEPENDENCIES}";
+		docker exec ${CONTAINER_NAME} add-apt-repository ppa:gift/pylint3 -y;
 	fi
+	docker exec ${CONTAINER_NAME} add-apt-repository ppa:gift/dev -y;
+
+	docker exec ${CONTAINER_NAME} apt-get update -q;
+
+	# Set locale to US English and UTF-8.
+	docker exec ${CONTAINER_NAME} locale-gen en_US.UTF-8;
+
+	# Install packages.
+	if test -n "${TOXENV}";
+	then
+		DPKG_PACKAGES="build-essential python${TRAVIS_PYTHON_VERSION} python${TRAVIS_PYTHON_VERSION}-dev tox";
+	else
+		DPKG_PACKAGES="";
+
+		if test "${TARGET}" = "coverage";
+		then
+			DPKG_PACKAGES="${DPKG_PACKAGES} curl git";
+
+		elif test "${TARGET}" = "jenkins2" || test "${TARGET}" = "jenkins3";
+		then
+			DPKG_PACKAGES="${DPKG_PACKAGES} sudo";
+
+		elif test ${TARGET} = "pylint";
+		then
+			DPKG_PACKAGES="${DPKG_PACKAGES} python3-distutils pylint";
+		fi
+		if test ${TRAVIS_PYTHON_VERSION} = "2.7";
+		then
+			DPKG_PACKAGES="${DPKG_PACKAGES} python ${DPKG_PYTHON2_DEPENDENCIES} ${DPKG_PYTHON2_TEST_DEPENDENCIES}";
+		else
+			DPKG_PACKAGES="${DPKG_PACKAGES} python3 ${DPKG_PYTHON3_DEPENDENCIES} ${DPKG_PYTHON3_TEST_DEPENDENCIES}";
+		fi
+	fi
+	docker exec -e "DEBIAN_FRONTEND=noninteractive" ${CONTAINER_NAME} sh -c "apt-get install -y ${DPKG_PACKAGES}";
 
 	docker cp ../artifacts ${CONTAINER_NAME}:/
-
-elif test ${TRAVIS_OS_NAME} = "linux" && test ${TARGET} != "jenkins";
-then
-	sudo rm -f /etc/apt/sources.list.d/travis_ci_zeromq3-source.list;
-
-	if test ${TARGET} = "pylint";
-	then
-		sudo add-apt-repository ppa:gift/pylint3 -y;
-	fi
-
-	sudo add-apt-repository ppa:gift/dev -y;
-	sudo apt-get update -q;
-
-	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
-	then
-		sudo apt-get install -y ${DPKG_PYTHON2_DEPENDENCIES} ${DPKG_PYTHON2_TEST_DEPENDENCIES};
-	else
-		sudo apt-get install -y ${DPKG_PYTHON3_DEPENDENCIES} ${DPKG_PYTHON3_TEST_DEPENDENCIES};
-	fi
-	if test ${TARGET} = "pylint";
-	then
-		sudo apt-get install -y pylint;
-	fi
 fi
