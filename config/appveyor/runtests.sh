93818fd4e0f6eddbc25f5f299f11b7a880ea4c9c
Joachim Metz <joachim.metz@gmail.com>
2025-11-20 18:36:58 +0100

    Applied updates and changes for CI tests (#650)

--- config/appveyor/runtests.sh
+++ config/appveyor/runtests.sh
@@ -2,14 +2,14 @@
 # Script to run tests
 
 # Set the following environment variables to build libyal with gettext.
-export CPPFLAGS="-I/usr/local/include -I/usr/local/opt/gettext/include ${CPPFLAGS}";
-export LDFLAGS="-L/usr/local/lib -L/usr/local/opt/gettext/lib ${LDFLAGS}";
+export CPPFLAGS="-I/usr/local/include -I/usr/local/opt/gettext/include ${CPPFLAGS}"
+export LDFLAGS="-L/usr/local/lib -L/usr/local/opt/gettext/lib ${LDFLAGS}"
 
 # Set the following environment variables to build pycrypto and yara-python.
-export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include ${CPPFLAGS}";
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib ${LDFLAGS}";
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include ${CPPFLAGS}"
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib ${LDFLAGS}"
 
-# Set the following environment variables to ensure tox can find Python 3.13.
-export PATH="/usr/local/opt/python@3.13/bin:${PATH}";
+# Set the following environment variables to ensure tox can find Python.
+export PATH="/usr/local/opt/python@${PYTHON_VERSION}/bin:${PATH}"
 
-tox -e py313
+tox -e py${PYTHON_VERSION//./}
